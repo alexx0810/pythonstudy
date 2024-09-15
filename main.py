cc813af83c8d03d50f09567e10d39d27f28012c5
Alex <alekspritula785@gmail.com>
2024-09-15 13:43:28 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+while True:
+    a = input('введіть повідомлення')
+    if not a:
+        print('ви нічого не ввели')
+    else:
+        print('ввели')
+        break
+while True:
+    age = input('введіть вік')
+
+    if not age:
+        print('ви нічого не ввели')
+    else:
+        if int(age) < 18:
+            print('неповнолітній')
+        else:
+            print('повнолітній')
+        break
